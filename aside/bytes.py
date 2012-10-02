e8f50925dc2f51f4fcf485c16e1048bb24fc1e72
Steve Kendall <kendall@wereware.com>
2012-10-02 17:19:05 -0400

    just about ready to sub in byte code

--- aside/bytes.py
+++ aside/bytes.py
@@ -14,14 +14,49 @@ sys.path.insert(0, os.path.join("..", "common"))
 from asciipixel import AsciiPixel 
 from screen import Screen 
 
-screen = Screen()
-#print str(screen)
-print str(screen.screen[0][0]) 
-fil = open("out2.bin", "wb") 
-msg = bytearray() 
-msg.extend(struct.pack("BB", screen.height, screen.width))  
-for row in screen.screen:
-	for asciipixel in row:
-		msg.extend(struct.pack("BB", asciipixel.ascii, asciipixel.color)) 
-fil.write(msg) 
-fil.close() 
+def writeScreen():
+	screen = Screen()
+	print str(screen)
+	fil = open("out2.bin", "wb") 
+	msg = bytearray() 
+	msg.extend(struct.pack("BB", screen.height, screen.width))  
+	for row in screen.screen:
+		for asciipixel in row:
+			msg.extend(struct.pack(
+					"BB", asciipixel.ascii, asciipixel.color)) 
+	fil.write(msg) 
+	fil.close() 
+
+def readScreen():
+	fil = open("out2.bin", "rb") 
+	msg = bytearray()
+	msg.extend(fil.read()) 	
+	fil.close() 
+	width = 0
+	height = 0
+	asciiPixels = [] 	
+	curRow = [] 
+	#print msg[0]
+	#print "len:%d" % len(str(msg[0]))
+	height, width = struct.unpack("BB", str(msg[:2])) 
+	print "height: %d, width %d" % (height, width)
+	for i in range(0, height):
+		# height and width are counting in ASCII PIXELS not bytes
+		for j in range(0, width):
+			curPos = 2 * (i * width + j) + 2 # 2=size of header 
+			symbol, color = struct.unpack("BB", 
+					str(msg[curPos:(curPos + 2)])) 
+			asciiPixel = AsciiPixel(symbol, color) 
+			curRow.append(asciiPixel)
+		asciiPixels.append(curRow)
+		curRow = [] 
+	screen = Screen(asciiPixels) 
+	return screen
+
+print "read screen:"
+read = readScreen()
+print str(read) 
+print "actual:"
+otherScreen = Screen()
+print str(otherScreen)
+
