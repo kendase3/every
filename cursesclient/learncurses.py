f7366d3f2cf0ff64eca1ba0ddd7c23ef66c6f6eb
Steve Kendall <kendall@wereware.com>
2012-10-05 06:39:46 -0400

    ok learnin

--- cursesclient/learncurses.py
+++ cursesclient/learncurses.py
@@ -3,9 +3,38 @@ import curses
 from curses import wrapper # handles the wonk automatically and enables colors 
 from curses import ascii # maybe? 
 
+#TODO: learn how to capture input, print colored cells, that's it!
+
+def respondToInput(screen):
+	c = screen.getch() 
+	if c == ord('y'):
+		screen.addstr(3, 0, "yussss", curses.color_pair(3))
+		return True
+	elif c == ord('n'):
+		screen.addstr(3, 0, "awww", curses.color_pair(1))
+		return True
+	elif c == ord('q'):
+		return False
+	else:
+		screen.addstr(3, 0, "?!?", curses.color_pair(2))
+		return True
+	
 def hello(screen):
-	screen.addstr("hello")
-	#print str(screen) 
+	#screen.attron(curses.A_BOLD)
+	screen.addstr("h", curses.A_BOLD)
+	screen.addstr("e", curses.A_BOLD)
+	screen.addstr("l", curses.A_BOLD)
+	screen.addstr("l", curses.A_BOLD)
+	screen.addstr("o", curses.A_BOLD)
+	curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK) 
+	curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK) 
+	curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK) 
+	screen.addstr(1, 0, '@', curses.color_pair(2)) 
+	screen.addstr(2, 0, '@', curses.color_pair(3)) 
+	#if curses.has_colors():
+	#	screen.addstr("y")
 	screen.refresh()
+	while respondToInput(screen):
+		pass 
 
 wrapper(hello)
