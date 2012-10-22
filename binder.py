c31054bdaa7023c32565da2b582f0690303bf76f
Steve Kendall <kendall@wereware.com>
2012-10-22 18:31:56 -0400

    have sendbuff, now just need a chat to send it to

--- binder.py
+++ binder.py
@@ -9,12 +9,14 @@ import sys
 ### SET THE PATH TO YOUR GAME FOLDER HERE
 # sys.path.insert(0, os.path.join("path", "to", "your", "folder")
 #sys.path.insert(0, os.path.join("..", "tag")) 
-sys.path.insert(0, os.path.join("..", "..", "ingress")) 
+#sys.path.insert(0, os.path.join("..", "..", "ingress")) 
+sys.path.insert(0, os.path.join("..", "..", "pop")) 
 # ~~~~~
 
 # ~~~~~
 ### IMPORT YOUR GAME CLASS HERE 
 # (must subclass Game class as found in server/game.py) 
 #from tag import Board  
-from battletyme import Arena
+#from battletyme import Arena
+from pop import GameMgr 
 # ~~~~~
