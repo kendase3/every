4e9e9c22f905125cbfaac63825d5a0a4214123ed
Steve Kendall <kendall@wereware.com>
2012-10-23 04:17:44 -0400

    added fix for green in cursesgfxmgr

--- client/cursesgfxmgr.py
+++ client/cursesgfxmgr.py
@@ -37,7 +37,7 @@ class CursesGfxMgr(IGfxMgr):
 		curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK) 
 		curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK) 
 		curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK) 
-		curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_BLACK) 
+		curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_BLACK) 
 		curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK) 
 		curses.init_pair(6, curses.COLOR_CYAN, curses.COLOR_BLACK) 
 	
